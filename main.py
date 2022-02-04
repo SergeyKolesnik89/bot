64ecdf66fff7b0870313cd75991d16aaf2c19272
SergeyKolesnik89 <83912868+SergeyKolesnik89@users.noreply.github.com>
2022-02-04 23:43:57 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import telebot
 
 
 TOKEN = "1094693261:AAERSOmcqWAp38SMxc6Wbou_S8wTQsuLu8s"
-APP_URL = f'https://belka418.herokuapp.com///{TOKEN}'zz
+APP_URL = f'https://belka418.herokuapp.com///{TOKEN}'
 #https://belka418.herokuapp.com/
 #bot = telebot.TeleBot("1094693261:AAERSOmcqWAp38SMxc6Wbou_S8wTQsuLu8s")
 bot = telebot.TeleBot(TOKEN)
