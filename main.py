61bd5db3114a926ecf743e89ab90e2380c413254
SergeyKolesnik89 <83912868+SergeyKolesnik89@users.noreply.github.com>
2022-02-04 22:21:42 +0600

    Update main.py

--- main.py
+++ main.py
@@ -17,20 +17,21 @@ import requests
 
 #для погоды
 
-from pyowm import OWM
-from pyowm.utils import config
-from pyowm.utils import timestamps
-import pyowm 
+
 
 
 #Дата и время
-
+import datetime as dt
+from PyQt5.QtCore import QTimer, QTime, Qt
+import time
+import datetime as dt
+import sys
 # Импортируем типы из модуля, чтобы создавать кнопки
 from telebot import types
 
 
 TOKEN = "1094693261:AAERSOmcqWAp38SMxc6Wbou_S8wTQsuLu8s"
-APP_URL = f'https://belka418.herokuapp.com///{TOKEN}'
+APP_URL = f'https://belka418.herokuapp.com///{TOKEN}'zz
 #https://belka418.herokuapp.com/
 #bot = telebot.TeleBot("1094693261:AAERSOmcqWAp38SMxc6Wbou_S8wTQsuLu8s")
 bot = telebot.TeleBot(TOKEN)
