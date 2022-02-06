4db504fdd06565095e4a69a465e0dc283fa26da0
SergeyKolesnik89 <83912868+SergeyKolesnik89@users.noreply.github.com>
2022-02-06 23:59:54 +0600

    Update main.py

--- main.py
+++ main.py
@@ -173,8 +173,8 @@ input()
    
 
 
-if __name__ == '__main__':
-   server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))           
+#if __name__ == '__main__':
+   #server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))           
 
 
 
