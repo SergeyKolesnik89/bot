eabee2c7ae814733bf998068ffb09956be6d9357
SergeyKolesnik89 <83912868+SergeyKolesnik89@users.noreply.github.com>
2022-02-07 23:47:16 +0600

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ APP_URL = f'https://belka418.herokuapp.com//{TOKEN}'
 #https://belka418.herokuapp.com/
 #bot = telebot.TeleBot("1094693261:AAERSOmcqWAp38SMxc6Wbou_S8wTQsuLu8s")
 bot = telebot.TeleBot(TOKEN)
-server = Flask(__name__)
+#server = Flask(__name__)
 
 
 
