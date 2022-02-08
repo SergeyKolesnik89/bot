a2279ca6e9ff133e0cf925445585d4ee353c2507
SergeyKolesnik89 <83912868+SergeyKolesnik89@users.noreply.github.com>
2022-02-08 23:54:52 +0600

    Update main.py

--- main.py
+++ main.py
@@ -101,14 +101,7 @@ def webhook():
    
 
 
-while True:
-    try:
-        bot.polling(none_stop=True)
 
-    except Exception as e:
-        print(e) 
-      
-        time.sleep(2)
 if __name__ == '__main__':
     server.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) 
 
