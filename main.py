bf9610052407afd1c015fcf02b236ba911ec0628
SergeyKolesnik89 <83912868+SergeyKolesnik89@users.noreply.github.com>
2022-02-09 00:20:49 +0600

    Update main.py

--- main.py
+++ main.py
@@ -63,6 +63,7 @@ def start(message):
         
 #Блок погоды
 @bot.message_handler(content_types=['today'])
+def today(message):
     URL = 'https://kakoysegodnyaprazdnik.ru/'
     HEADERS = {
         'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36'
