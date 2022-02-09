f83269bb525ddac62ee3611769bd1fa19cba57ab
SergeyKolesnik89 <83912868+SergeyKolesnik89@users.noreply.github.com>
2022-02-09 23:21:03 +0600

    Update main.py

--- main.py
+++ main.py
@@ -49,8 +49,8 @@ owm.config["language"] = "ru"
 
 keyboard1 = types.InlineKeyboardMarkup()
 keyboard1 = telebot.types.ReplyKeyboardMarkup()
-keyboard1.row('Анекдот', 'weather')
-keyboard1.row( 'today', 'О разработчиках')
+keyboard1.row('Анекдот', 'Погода')
+keyboard1.row( 'В этот день', 'О разработчиках')
 #keyboard1.row('Гороскоп', 'В этот день', 'О разработчиках')
 
 
