41aabae3d28875ee04d3c4d4d8b5c13b123d3008
SergeyKolesnik89 <83912868+SergeyKolesnik89@users.noreply.github.com>
2022-02-11 23:43:09 +0600

    Update main.py

--- main.py
+++ main.py
@@ -134,15 +134,45 @@ def jokes_text(message):
             
 elif message.text.lower() == 'погода':
         
-    bot.send_message(message.chat.id,'Введите город . . . ')
+    bot.send_message(message.from_user.id,'Введите город . . . ')
        
         
         
            
            
     #try:
-        
-        
+    if message.text == "привет" or message.text == "Привет":
+        pass
+    else:
+        mgr = owm.weather_manager()
+        observation = mgr.weather_at_place(message.text)
+            
+            
+            
+            
+        w = observation.weather
+            
+        temp = w.temperature('celsius')["temp"]
+                   
+           
+             
+        answer = (f'В городе {message.text} сейчас { w.detailed_status } '"\n")
+            
+        bot.send_message(message.chat.id, answer)
+            
+        answer = (f'Температура сейчас в районе  {temp}  градусов Цельсия' "\n\n")
+        bot.send_message(message.chat.id, answer)
+            
+        if temp <10:
+            answer = "На улице холодно, одевайся очень тепло"
+                
+        elif temp <20:
+            answer = "Сейчас прохладно, одевайся теплее"
+                
+        elif temp > 20:
+            answer = "Надевай что хочешь, там тепло"
+                
+        bot.send_message(message.chat.id, answer)        
   
             
             
