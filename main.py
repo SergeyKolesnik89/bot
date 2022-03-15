af6c48a6426fa23c86e399fd4b61aad0d26da044
SergeyKolesnik89 <83912868+SergeyKolesnik89@users.noreply.github.com>
2022-03-15 23:25:17 +0600

    Update main.py

--- main.py
+++ main.py
@@ -140,16 +140,15 @@ def jokes_text(message):
         #text = str(input('>>>   '))
     #elif message.text.lower():
     
-    def repeat_all_messages(message): # Название функции не играет никакой роли
-        bot.send_message(message.chat.id, message.text)
-            
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли
+    bot.send_message(message.chat.id, message.text)
         
 
 
 
 
-        
-
+      
        
             
 
