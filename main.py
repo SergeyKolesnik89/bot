6ba6664683b055ad340cf42a4394921df572fe10
SergeyKolesnik89 <83912868+SergeyKolesnik89@users.noreply.github.com>
2022-03-15 22:39:10 +0600

    Update main.py

--- main.py
+++ main.py
@@ -138,34 +138,7 @@ def jokes_text(message):
         bot.send_message(message.from_user.id, "Здравствуйте. Вы можете узнать здесь погоду. Просто напишите название города." + "\n")
         
         
-        #text = str(input('>>>   '))
-        if message.text == (text):
-            URL = ('https://www.google.com/search?q=%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0 ' + text)
-            HEADERS = {
-                'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36'
-            }
-            response = requests.get(URL, headers = HEADERS)
-            soup = BeautifulSoup(response.content, 'html.parser')
-
-            items = soup.findAll('div', class_="nawv0d")#вывод региона
-
-            comps = []
-            print (URL)
-            for item in items:
-                comps.append({
-                'title' : item.find('div', class_="wob_loc q8U8x").get_text(strip = True),
-                'temperature' : item.find('span', class_="wob_t q8U8x").get_text(strip = True),
-                'weather' : item.find('div', class_="wob_dcp").get_text(strip = True)
-
-
-                })
-
-            global comp
-            for comp in comps:
-                bot.send_message(message.from_user.id,'В городе ' + f'{comp["title"]}\nТемпература  {comp["temperature"]} град. Цельсия\n{comp["weather"]} ')
-    
-               
-       
+        
 
 
 
