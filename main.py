d02d77319d3c15cc60d32068fc9485b72fda13e3
SergeyKolesnik89 <83912868+SergeyKolesnik89@users.noreply.github.com>
2022-03-16 00:05:13 +0600

    Update main.py

--- main.py
+++ main.py
@@ -147,7 +147,8 @@ def jokes_text(message):
 
     else: 
         #bot.send_message(message.from_user.id, message.text)
-        URL = ('https://www.google.com/search?q=%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0 ' + message.text)
+        URL = ('https://www.google.com/search' + 'погода в' + message.text)
+        #URL = ('https://www.google.com/search?q=%D0%BF%D0%BE%D0%B3%D0%BE%D0%B4%D0%B0 ' + message.text)
         HEADERS = {
             'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36'
         }
